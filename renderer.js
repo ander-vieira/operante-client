66668dd5589b39d9f5148ec16e4f1f6d670252b7
Ander Vieira <ander@ideable.net>
2018-01-18 16:19:36 +0100

    Prueba de cookies

--- renderer.js
+++ renderer.js
@@ -3,10 +3,11 @@
 // All of the Node.js APIs are available in this process.
 
 var http = require("http");
+const restConfig = require("./rest.json");
 
 function cargarDatosUsuario(callback) {
   var request = http.request({
-    host: "operante.herokuapp.com",
+    host: restConfig.domain,
     port: 80,
     path: "/rest/datosUsuario",
     method: "GET"
@@ -21,6 +22,17 @@ function cargarDatosUsuario(callback) {
 
   request.end();
 }
+
+var window = require('electron').remote.getCurrentWindow();
+const cookie = {url: restConfig.proto+'://'+restConfig.domain, name: 'session', value: 'asdf'}
+window.webContents.session.cookies.set(cookie,
+  function(error) {
+    if (error) console.error(error)
+});
+
+window.webContents.session.cookies.get({url: restConfig.proto+'://'+restConfig.domain}, (error, cookies) => {
+  $(".nombreCookie").text(cookies[0].name+" "+cookies[0].value);
+});
 
 cargarDatosUsuario(function(nombre) {
   $(".nombreUsuario").text(nombre);
