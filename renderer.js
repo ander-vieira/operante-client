7c2be0158de2815c1f2f621dc6401e74219f2da3
Ander Vieira <ander@ideable.net>
2018-01-18 15:17:58 +0100

    Peticion AJAX al servidor

--- renderer.js
+++ renderer.js
@@ -1,3 +1,27 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var http = require("http");
+
+function cargarDatosUsuario(callback) {
+  var request = http.request({
+    host: "operante.herokuapp.com",
+    port: 80,
+    path: "/rest/datosUsuario",
+    method: "GET"
+  }, function(response) {
+    response.on('data', function(chunk) {
+      var data = JSON.parse(chunk.toString("utf8"));
+      callback(data.nombre);
+    })
+  }).on("error", function() {
+      callback("ERROR");
+  });
+
+  request.end();
+}
+
+cargarDatosUsuario(function(nombre) {
+  $(".nombreUsuario").text(nombre);
+});
